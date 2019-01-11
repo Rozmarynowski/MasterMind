4fda052e819eddcffdd76b44562e6c2d0b21cf16
Rozmarynowski <rozmarynowski_adam@onet.pl>
2019-01-11 11:55:55 +0100

    Added "choose code" without repetition algorythm

---
+++ src/engine/CodeChooser.java
@@ -0,0 +1,33 @@
+package engine;
+
+public class CodeChooser {
+
+    private int click;
+    private String color;
+    Colors colors;
+
+    public String setColor(int click){
+        this.click = click;
+
+        if(click == 1){
+            color = colors.RED.getColorName();
+        }else if (click ==2){
+            color = colors.GREEN.getColorName();
+        }else if (click ==3){
+            color = colors.BLUE.getColorName();
+        }else if (click ==4){
+            color = colors.YELLOW.getColorName();
+        }else if (click ==5){
+            color = colors.PURPLE.getColorName();
+        }else if (click ==6) {
+            color = colors.PINK.getColorName();
+        }
+
+
+
+            return color;
+    }
+
+
+
+}
