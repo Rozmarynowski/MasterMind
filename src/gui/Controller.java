555b68c272e9b761aad4eaadbc35e0c4d51d72c1
Rozmarynowski <rozmarynowski_adam@onet.pl>
2019-01-11 09:28:22 +0100

    first_gui
    Made first idea of GUI.
    Added fundamental functions.

---
+++ src/gui/Controller.java
@@ -0,0 +1,34 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
+public class Controller {
+
+    @FXML
+    private Button coderOne;
+
+    @FXML
+    private Button coderTwo;
+
+    @FXML
+    private Button coderThree;
+
+    @FXML
+    private Button codeFour;
+
+
+    public void colorCodeFour(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void colorCodeThree(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void colorCodeTwo(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void colorCodeOne(javafx.event.ActionEvent actionEvent) {
+    }
+}
